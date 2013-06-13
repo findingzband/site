e0393d0fbd931a5048f553204f98460365af803d
Christopher Miller <cmiller0189@gmail.com>
2013-06-12 22:08:34 -0500

    Multiple albums with shuffling, refactoring, config

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,45 @@
-var BANDCAMP_API_KEY = "droghagspakrrynavortishegnir";
-var FLICKR_API_KEY = "96184c3c741e46bebc7b413055aec46c";
-var ALBUM_ID = "2630216186";
-var ROOT_URL = "http://music.findingz.com";
-var AMAZON_URL = "http://www.amazon.com/gp/product/";
 var tracks = [];
 var current = 0;
-var amazonSingles = {
-    "Better Off Dead": "B0050TKR0Y",
-    "Just Trust": "B0050TKR54",
-    "Mr. Wrong": "B0050TKRDQ",
-    "Envy": "B0050TKRJK",
-    "Throw Away": "B0050TKRRM",
-    "Gravel Roads": "B0050TKS08",
-    "Without Me": "B0050TKS6W",
-    "Games": "B0050TKSN0",
-    "Relapse": "B0050TKT02",
-    "500": "B0050TKTFW",
-    "Black Notebook": "B0050TKUAG",
-    "Past Beyond the Point": "B0050TKUSI",
-    "Blue": "B0050TKV0K",
-    "Another Life": "B0050TKV50"
-}
-var colorBoxDefaults = {
-    current: "{current} of {total}",
-    maxHeight: "90%",
-    opacity: .6,
-    slideshow: true,
-    slideshowAuto: false,
-    slideshowStart: "Start Slideshow",
-    slideshowStop: "Stop Slideshow"
-}
-var fleeckrDefaults = {
-    apikey: FLICKR_API_KEY,
-    limit: 40,
-    method: "flickr.photosets.getPhotos"
-}
-var flickrSets = {
-    artwork: "72157625336005640",
-    live: "72157625015020630",
-    promotional: "72157624890427357"
+var config = {
+    bandcamp_api_key: "droghagspakrrynavortishegnir",
+    albums: [{id: "1223951139", shuffle: false}, {id: "2630216186", shuffle: true}],
+    bandcamp_url: "http://music.findingz.com",
+    amazon_url: "http://www.amazon.com/gp/product/",
+    amazon_tracks: {
+        "Better Off Dead": "B0050TKR0Y",
+        "Just Trust": "B0050TKR54",
+        "Mr. Wrong": "B0050TKRDQ",
+        "Envy": "B0050TKRJK",
+        "Throw Away": "B0050TKRRM",
+        "Gravel Roads": "B0050TKS08",
+        "Without Me": "B0050TKS6W",
+        "Games": "B0050TKSN0",
+        "Relapse": "B0050TKT02",
+        "500": "B0050TKTFW",
+        "Black Notebook": "B0050TKUAG",
+        "Past Beyond the Point": "B0050TKUSI",
+        "Blue": "B0050TKV0K",
+        "Another Life": "B0050TKV50"
+    },
+    colorBoxDefaults: {
+        current: "{current} of {total}",
+        maxHeight: "90%",
+        opacity: .6,
+        slideshow: true,
+        slideshowAuto: false,
+        slideshowStart: "Start Slideshow",
+        slideshowStop: "Stop Slideshow"
+    },
+    fleeckrDefaults: {
+        apikey: "96184c3c741e46bebc7b413055aec46c",
+        limit: 40,
+        method: "flickr.photosets.getPhotos"
+    },
+    flickrSets: {
+        artwork: "72157625336005640",
+        live: "72157625015020630",
+        promotional: "72157624890427357"
+    }
 }
 
 function checkBrowser()
@@ -64,14 +65,14 @@ function initPlayer(track)
     $("#player").html("");
     var loaded = false;
     var manualSeek = false;
-    var player = '\
+    var $player = $('\
         <p class="player">\
             <a href="http://music.findingz.com"><img class="album hasTooltip" title="Buy Make It Right on Bandcamp" src="http://s0.bcbits.com/img/buttons/bandcamp_60x60_white.png"></a>\
             <span class="previous" />\
             <span class="playtoggle" />\
             <span class="next" />\
             <span class="trackname"><a href="'+track.view_url+'" target="_blank" title="View track information for \''+track.name+'\' on Bandcamp.">'+track.name+'</a></span>\
-            <span class="amazonLink"><a href="'+track.amazon_url+'" target="_blank" title="Buy \''+track.name+'\' on Amazon."><img src="static/images/social/amazon_buy.png" /></a></span>\
+            <span class="amazonLink"></span>\
             <span class="gutter">\
                 <span class="loading" />\
                 <span class="ui-slider-handle handle" />\
@@ -80,8 +81,10 @@ function initPlayer(track)
             <audio preload="metadata">\
             <source src="'+track.stream_url+'" type="audio/mpeg"></source>\
             </audio>\
-        </p>';
-    $("#player").append(player);
+        </p>');
+    if (track.amazon_url)
+        $player.find(".amazonLink").html('<a href="'+track.amazon_url+'" target="_blank" title="Buy \''+track.name+'\' on Amazon."><img src="static/images/social/amazon_buy.png" /></a>');
+    $("#player").append($player);
     var audio = $(".player audio").get(0);
     var loadingIndicator = $(".player .loading");
     var positionIndicator = $(".player .handle");
@@ -202,83 +205,82 @@ function tipsyfy()
         opacity: 0.9
     });
 }
-jQuery(document).ready(function() {
-    $.getJSON('http://api.bandcamp.com/api/album/1/info?key='+BANDCAMP_API_KEY+'&album_id='+ALBUM_ID+'&callback=?',
-    function(returnData) {
-        if (checkBrowser()) { // Chrome, Safari, IE9
-            $.each(returnData.tracks, function() {
-                tracks.push({
-                    name: this.title,
-                    stream_url: this.streaming_url,
-                    view_url: ROOT_URL + this.url,
-                    amazon_url: AMAZON_URL + amazonSingles[this.title]
+
+$(document).ready(function() {
+    
+    //Music Player
+    if (checkBrowser()) { // Chrome, Safari, IE9
+        
+        var deferreds = [];
+        
+        for (var i=0; i<config.albums.length; i++) {
+            
+            var album = config.albums[i];
+            
+            deferreds.push($.getJSON('http://api.bandcamp.com/api/album/1/info?key='+config.bandcamp_api_key+'&album_id='+album.id+'&callback=?', function(returnData) {
+                
+                var albumTracks = [];
+                            
+                $.each(returnData.tracks, function() {
+                    var temp = {
+                        name: this.title,
+                        stream_url: this.streaming_url,
+                        view_url: config.bandcamp_url + this.url
+                    }
+                    if (config.amazon_tracks[this.title])
+                        temp.amazon_url = config.amazon_url + config.amazon_tracks[this.title];
+                    albumTracks.push(temp);
                 });
-            });
-            tracks.sort(function() { return Math.round(Math.random())-0.5; });
-            var throwAwayIndex, blueIndex;
-            for (var x=0; x<=tracks.length; x++) {
-            	if (tracks[x].name === "Throw Away") {
-            		throwAwayIndex = x;
-            		break;
-            	}
-            }
-            tracks.move(throwAwayIndex, 0);
-            for (var x=0; x<=tracks.length; x++) {
-            	if (tracks[x].name === "Blue") {
-            		blueIndex = x;
-            		break;
-            	}
-            }
-            tracks.move(blueIndex, 1);
+                            
+                if (album.shuffle)
+                    albumTracks.sort(function() { return Math.round(Math.random())-0.5; });
+                
+                tracks = tracks.concat(albumTracks);
+            }));
+        }
+        
+        $.when.apply($, deferreds).done(function () {
             initPlayer(tracks[0]);
-        }
-        else { // Firefox, IE6-8
-            var flashURL = '\
-                <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="80">\
-                    <param name="movie" value="http://bandcamp.com/EmbeddedPlayer.swf/album='+ALBUM_ID+'/size=grande/bgcol=000000/linkcol=6FC334/" />\
-                    <param name="quality" value="high" />\
-                    <param name="allowScriptAccess" value="never" />\
-                    <param name="allowNetworking" value="always" />\
-                    <param name="wmode" value="transparent" />\
-                    <param name="bgcolor" value="#000000" />\
-                    <embed src="http://bandcamp.com/EmbeddedPlayer.swf/album='+ALBUM_ID+'/size=grande/bgcol=000000/linkcol=6FC334/" width="300" height="80" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" quality=high allowScriptAccess=never allowNetworking=always wmode=transparent bgcolor=#000000 ></embed>\
-                    <noembed><a href="http://music.findingzband.com/">Make It Right by Finding Z</a></noembed>\
-                </object><br />';
-            $("#player").append(flashURL);
-        }
-    });
-    $("#photosArtwork").jfleeckr($.extend(fleeckrDefaults, {
-        extra: "photoset_id=" + flickrSets.artwork,
+        });
+        
+    } else { // Firefox, IE6-8
+        var flashURL = '\
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="80">\
+                <param name="movie" value="http://bandcamp.com/EmbeddedPlayer.swf/album='+config.albums[0].id+'/size=grande/bgcol=000000/linkcol=6FC334/" />\
+                <param name="quality" value="high" />\
+                <param name="allowScriptAccess" value="never" />\
+                <param name="allowNetworking" value="always" />\
+                <param name="wmode" value="transparent" />\
+                <param name="bgcolor" value="#000000" />\
+                <embed src="http://bandcamp.com/EmbeddedPlayer.swf/album='+config.albums[0].id+'/size=grande/bgcol=000000/linkcol=6FC334/" width="300" height="80" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" quality=high allowScriptAccess=never allowNetworking=always wmode=transparent bgcolor=#000000 ></embed>\
+                <noembed><a href="http://music.findingzband.com/">Make It Right by Finding Z</a></noembed>\
+            </object><br />';
+        $("#player").append(flashURL);
+    }
+    
+    // Photos
+    $("#photosArtwork").jfleeckr($.extend(config.fleeckrDefaults, {
+        extra: "photoset_id=" + config.flickrSets.artwork,
         itemTemplate: '<div class="fleeckr-image"><a href="{{image_b}}" title="{{title}}" rel="artworkGallery"><img alt="{{title}}" src="{{image_s}}" /></div>'
     }), function(data) {
-        $('#photosArtwork a').colorbox(colorBoxDefaults);
+        $('#photosArtwork a').colorbox(config.colorBoxDefaults);
         hidePhotos($(this));
     });
-    $("#photosLive").jfleeckr($.extend(fleeckrDefaults, {
-        extra: "photoset_id=" + flickrSets.live,
+    $("#photosLive").jfleeckr($.extend(config.fleeckrDefaults, {
+        extra: "photoset_id=" + config.flickrSets.live,
         itemTemplate: '<div class="fleeckr-image"><a href="{{image_b}}" title="{{title}}" rel="liveGallery"><img alt="{{title}}" src="{{image_s}}" /></div>'
     }), function(data) {
-        $('#photosLive a').colorbox(colorBoxDefaults);
+        $('#photosLive a').colorbox(config.colorBoxDefaults);
         hidePhotos($(this));
     });
-    $("#photosPromotional").jfleeckr($.extend(fleeckrDefaults, {
-        extra: "photoset_id=" + flickrSets.promotional,
+    $("#photosPromotional").jfleeckr($.extend(config.fleeckrDefaults, {
+        extra: "photoset_id=" + config.flickrSets.promotional,
         itemTemplate: '<div class="fleeckr-image"><a href="{{image_b}}" title="{{title}}" rel="promotionalGallery"><img alt="{{title}}" src="{{image_s}}" /></div>'
     }), function(data) {
-        $('#photosPromotional a').colorbox(colorBoxDefaults);
+        $('#photosPromotional a').colorbox(config.colorBoxDefaults);
         hidePhotos($(this));
     });
     $("#photosWrapper").hide();
     disableSelection(document.body);
     tipsyfy();
 });
-Array.prototype.move = function (old_index, new_index) {
-    if (new_index >= this.length) {
-        var k = new_index - this.length;
-        while ((k--) + 1) {
-            this.push(undefined);
-        }
-    }
-    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-    return this; // for testing purposes
-};
